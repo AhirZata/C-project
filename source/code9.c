10f7177eb5d0d9137c6a8d9ab3a8c8bcc4900b64
ahir <heypeyw@gmail.com>
2025-04-30 15:12:49 +0330

    add main code to repo/set some changes!

--- source/code9.c
+++ source/code9.c
@@ -2,11 +2,17 @@
 
 int main () {
 
+    
+    //"define some variable"
     int clock = 0;
 
+
+    //"getting some input from user"
     printf("what time is it: (0 or 24):");
     scanf("%d", &clock);
 
+
+    //"set condition and print result!"
     if(clock >=7 && clock <=17){
         printf("The air is day\n");
     }
@@ -16,5 +22,7 @@ int main () {
     else {
         printf("The number that you enter is not valid!\n");
     }
-    return 0;
+
+
+return 0;
 }
